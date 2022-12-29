3d4ae2e3cc5a8b4877bf5f85f9bd8721b8bace54
abdelrahman115689 <121627903+abdelrahman115689@users.noreply.github.com>
2022-12-29 17:53:51 +0200

    Add files via upload

---
+++ perfect/perfect/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Please enter first number");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please enter second number");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Perfect results :");
+{
+    if (num1 >= 0 && num2 >= 6 && num1 < 7)
+        Console.WriteLine("6");
+    if (num2 >= 28 && num1 <= 28)
+        Console.WriteLine("28");
+    if (num2 >= 496 && num1 <= 496)
+        Console.WriteLine("496");
+    if (num2 >= 8128 && num1 <= 8128)
+        Console.WriteLine("8128");
+    if (num2 >= 33550336 && num1 <= 33550336)
+        Console.WriteLine("33550336");
+}
---
+++ prrime/prrime/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Please enter first number");
+int prime = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please enter other number");
+int prime1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Prime result is :");
+
+int h = 0;
+
+for (int i = prime; i <= prime1; i++)
+{
+    for (int w = 1; w <= i; w++)
+    {
+        if (i % w == 0)
+        {
+            h++;
+        }
+    }
+    if (h == 2)
+    {
+        Console.WriteLine(i);
+    }
+    h = 0;
+}
